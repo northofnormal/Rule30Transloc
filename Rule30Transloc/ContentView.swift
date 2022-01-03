1e31b364af78232c0f7de6e5eb8d7893181047d4
Anne Cahalan <northofnormal@gmail.com>
2022-01-03 12:56:32 -0500

    Basic algorithm

--- Rule30Transloc/ContentView.swift
+++ Rule30Transloc/ContentView.swift
@@ -8,9 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var patternRows = [[Int]]()
+    @State private var sizeOfPattern = 3
+
     var body: some View {
         Text("Hello, world!")
             .padding()
+    }
+
+    func rule30ify(_ maxSize: Int) {
+        guard let middle = findMiddle(maxSize) else { return }
+
+        var startingArray = [Int]()
+        startingArray.append(contentsOf: repeatElement(0, count: maxSize))
+
+        for location in middle {
+            startingArray[location] = 1
+        }
+
+        patternRows = newRule30Array(from: startingArray)
+    }
+
+    func findMiddle(_ size: Int) -> [Int]? {
+        let half = size/2
+
+        if size % 2 == 0 {
+            return [half - 1, half, half + 1]
+        } else {
+            return [half]
+        }
+    }
+
+    func newRule30Array(from starter: [Int]) -> [[Int]] {
+        var totalResult = [[Int]]()
+        totalResult.append(starter)
+        var newTranslatedArray = translate(starter)
+        totalResult.append(newTranslatedArray)
+
+        for _ in 1...starter.count - 1  {
+            newTranslatedArray = translate(newTranslatedArray)
+            totalResult.append(newTranslatedArray)
+        }
+
+        return totalResult
+    }
+
+    func translate(_ array: [Int]) -> [Int] {
+        var result = [Int]()
+        var previous = 0
+        var following = 0
+
+        for (index, item) in array.enumerated() {
+
+            if index == 0 {
+                previous = determineValue(from: [0, 0, item])
+            } else {
+                previous = array[index - 1]
+            }
+
+            if index == array.count - 1 {
+                following = determineValue(from: [item, 0, 0])
+            } else {
+                following = array[index + 1]
+            }
+
+            let newArray = [previous, item, following]
+            result.append(determineValue(from: newArray))
+        }
+
+        return result
+    }
+
+    func determineValue(from array:[Int]) -> Int {
+        if array == [1,1,1] || array == [1,1,0] || array == [1,0,1] || array == [0,0,0] {
+            return 0
+        }
+
+        if array == [1,0,0] || array == [0,1,0] || array == [0,1,1] || array == [0,0,1] {
+            return 1
+        }
+
+        return 3
     }
 }
 
