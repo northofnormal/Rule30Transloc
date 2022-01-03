1b59f03da5e6b38178d911978e9a9a9d0d1ec595
Anne Cahalan <northofnormal@gmail.com>
2022-01-03 12:54:22 -0500

    Initial Commit

---
+++ Rule30Transloc/Rule30TranslocApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rule30TranslocApp.swift
+//  Rule30Transloc
+//
+//  Created by Anne Cahalan on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Rule30TranslocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
